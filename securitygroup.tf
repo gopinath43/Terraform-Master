759166f901d814cb198d9eca36ce30a912687fe5
gopinath43 <39530198+gopinath43@users.noreply.github.com>
2019-01-24 00:48:19 +0530

    Update securitygroup.tf
    Few changes...

--- securitygroup.tf
+++ securitygroup.tf
@@ -60,60 +60,59 @@ resource "aws_security_group" "APP01_SG" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-# ingress {
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
+  ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#ingress {
-#    from_port   = 443
-#    to_port     = 443
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
+ ingress {
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
 
-ingress {
+  ingress {
+     from_port   = 0
+     to_port     = 65535
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+ ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     security_groups = ["${aws_security_group.WEB01_SG.id}"]
   }
-#egress {
-#    from_port   = 3306
-#    to_port     = 3306
-#    protocol    = "tcp"
-#    security_groups = ["${aws_security_group.DB01_SG.id}"]
-# }
+ egress {
+     from_port   = 3306
+     to_port     = 3306
+     protocol    = "tcp"
+     security_groups = ["${aws_security_group.DB01_SG.id}"]
+  }
+  egress {
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     security_groups = ["${aws_security_group.DB01_SG.id}"]
+  }
+   egress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "APP01_SG"
-  }
-}
-
-# Below are the DB01 security group details:
-
-resource "aws_security_group" "DB01_SG" {
-  vpc_id      = "${aws_vpc.main.id}"
-  name        = "DB01_SG"
-  description = "security group inbound traffic for DB01 instance"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = ["${aws_security_group.APP01_SG.id}"]
-  }
-
-  tags = {
-    Name = "DB01_SG"
   }
 }
 
@@ -136,9 +135,68 @@ egress {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
    tags = {
     Name = "AM01_SG"
   }
 }  
+
+# Below are the DB01 security group details:
+
+resource "aws_security_group" "DB01_SG" {
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "DB01_SG"
+  description = "security group inbound traffic for DB01 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = ["${aws_security_group.APP01_SG.id}"]
+  }
+
+egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
+egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+egress {
+     from_port   = 0
+     to_port     = 65535
+     protocol    = "tcp"
+	 security_groups = ["${aws_security_group.APP01_SG.id}"]
+  }
+  
+  tags = {
+    Name = "DB01_SG"
+  }
+}
+
   
